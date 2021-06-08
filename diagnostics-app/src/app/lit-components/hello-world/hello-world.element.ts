cf60334a6a6e60b0bc334e4aff257f97915f1479
Aayush Joglekar <aayushjog@gmail.com>
2021-06-08 21:43:05 +0530

    [app] Add custom CSS Theme

--- diagnostics-app/src/app/lit-components/hello-world/hello-world.element.ts
+++ diagnostics-app/src/app/lit-components/hello-world/hello-world.element.ts
@@ -4,12 +4,25 @@
  * found in the LICENSE file.
  */
 
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 @customElement('hello-world')
 export class HelloWorldElement extends LitElement {
-  render() {
-    return html` <h1>Hello World from LitElement!</h1> `;
+  static styles = css`
+    p {
+      background-color: var(--theme-light);
+      color: var(--theme-primary);
+      padding: var(--spacing-m) 0;
+      font-size: var(--size-l);
+      display: flex;
+      justify-content: center;
+      border-bottom: .5rem solid var(--theme-accent);
+      border-top: .3rem solid var(--theme-black);
+    }
+  `;
+
+  protected render() {
+    return html` <p>Hello World from LitElement!</p> `;
   }
 }
